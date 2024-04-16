4b277b2b8afc60f2a0eabe466a72b2d843ac2aab
Sembo Sadur <labmailssadur@gmail.com>
2024-04-16 22:38:00 +0200

    Add License header to Makefile, remove useless TODO

--- Zitatespucker/Zitatespucker.h
+++ Zitatespucker/Zitatespucker.h
@@ -41,7 +41,6 @@
 
 // TODO:
 // Tests (for error cases etc)
-// Integrity verification of struct?
 // "NOFEATURES" preproc flag to stop compilation when nothing is enabled?
 // UIs: Nuklear (also test on Windows); Win32
 // SQL (https://www.sqlite.org/)
