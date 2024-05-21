2dc186a8d78f05ec007237ba246533552556b17c
Sembo Sadur <labmailssadur@gmail.com>
2024-05-21 23:14:06 +0200

    ZitatespuckerSQL: implement static functions

--- src/Zitatespucker_sqlite.c
+++ src/Zitatespucker_sqlite.c
@@ -21,6 +21,10 @@
 
 /* Standard headers */
 #include <stdio.h>
+#include <limits.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
 
 
 /* SQLite headers */
@@ -40,6 +44,14 @@
 	This function allocates, and the given object must be freed with ZitatespuckerZitatFree().
 */
 static ZitatespuckerZitat *ZitatespuckerSQLGetPopulatedStruct(sqlite3_stmt *ZitatStmt);
+
+/*
+	Get the string content of column iCol from prepared statement ZitatStmt.
+
+	NULL may be a valid return.
+	This function duplicates the string and check for proper NULL-termination.
+*/
+static inline char *ZitatespuckerSQLGetStringAllocated(sqlite3_stmt *ZitatStmt, int iCol);
 
 
 /* Externally callable */
@@ -119,3 +131,91 @@ ZitatespuckerZitat *ZitatespuckerSQLGetZitatAllFromFile(const char *filename)
 
 
 /* Static function definitions */
+
+static ZitatespuckerZitat *ZitatespuckerSQLGetPopulatedStruct(sqlite3_stmt *ZitatStmt)
+{
+	// mucho importante: SQLite type coercion table
+	ZitatespuckerZitat *Zitat;
+	if ((Zitat = (ZitatespuckerZitat *) malloc(sizeof(ZitatespuckerZitat))) == NULL) {
+		(void) fprintf(stderr, "%s:%d:%s: malloc() returned NULL.\n", __FILE__, __LINE__, __func__);
+		return NULL;
+	}
+	// init
+	ZitatespuckerZitatInit(Zitat);
+
+	// author
+	Zitat->author = ZitatespuckerSQLGetStringAllocated(ZitatStmt, 0);
+
+	// zitat
+	Zitat->zitat = ZitatespuckerSQLGetStringAllocated(ZitatStmt, 1);
+
+	// comment
+	Zitat->comment = ZitatespuckerSQLGetStringAllocated(ZitatStmt, 2);
+
+	// day
+	int32_t tmpInt = sqlite3_column_int(ZitatStmt, 3);
+	if (tmpInt < 0)
+		tmpInt = 0;
+	else if (tmpInt > UINT8_MAX)
+		tmpInt = UINT8_MAX;
+	Zitat->day = (uint8_t) tmpInt;
+
+	// month
+	tmpInt = sqlite3_column_int(ZitatStmt, 4);
+	if (tmpInt < 0)
+		tmpInt = 0;
+	else if (tmpInt > UINT8_MAX)
+		tmpInt = UINT8_MAX;
+	Zitat->month = (uint8_t) tmpInt;
+
+	// year
+	tmpInt = sqlite3_column_int(ZitatStmt, 5);
+	if (tmpInt < 0)
+		tmpInt = 0;
+	else if (tmpInt > UINT16_MAX)
+		tmpInt = UINT16_MAX;
+	Zitat->year = (uint16_t) tmpInt;
+
+	// annodomini
+	size_t bytelen = sqlite3_column_bytes(ZitatStmt, 6);
+	if (bytelen != 5)
+		Zitat->annodomini = false;
+	else {
+		const unsigned char *tmpS = sqlite3_column_text(ZitatStmt, 6);
+		if (tmpS == NULL) // only on OOM
+			Zitat->annodomini = false;
+		else if (strncmp(tmpS, "true", 5) == 0)
+			Zitat->annodomini = true;
+		else
+			Zitat->annodomini = false;
+	}
+
+	return Zitat;
+}
+
+static inline char *ZitatespuckerSQLGetStringAllocated(sqlite3_stmt *ZitatStmt, int iCol)
+{
+	size_t bytelen = sqlite3_column_bytes(ZitatStmt, iCol);
+	if (bytelen == 0)
+		return NULL;
+	
+	const unsigned char *tmpS = sqlite3_column_text(ZitatStmt, iCol);
+	if (tmpS == NULL) // happens only on OOM, theoretically
+		return NULL;
+	
+	if (tmpS[bytelen - 1] == '\0') {
+		// yay
+		char *ret = (char *) malloc(bytelen * sizeof(char));
+		if (ret != NULL)
+			(void) strcpy(ret, tmpS);
+		return ret;
+	} else {
+		// oof, can happen if you try to get BLOB as TEXT
+		char *ret = (char *) malloc((bytelen + 1) * sizeof(char));
+		if (ret != NULL) {
+			(void) strncpy(ret, tmpS, bytelen);
+			ret[bytelen] = '\0';
+		}
+		return ret;
+	}
+}
