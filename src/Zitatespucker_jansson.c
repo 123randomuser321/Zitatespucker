c09f45583ea17f20eb04745bbe764c8883de8076
Sembo Sadur <labmailssadur@gmail.com>
2024-06-16 17:19:30 +0200

    Zitatespucker_jansson: implement various, shift some stuff

--- src/Zitatespucker_jansson.c
+++ src/Zitatespucker_jansson.c
@@ -28,9 +28,85 @@
 #include "Zitatespucker/Zitatespucker_jansson.h"
 
 
+/* Static function declarations */
+
+/*
+	Get a json_t of type array from filename, using the predefined key.
+	Returns NULL on error.
+
+	The returned (non-NULL) object must be freed using json_decref().
+*/
+static json_t *ZitatespuckerJSONGetZitatArrayFromFile(const char *filename);
+
+/*
+	Returns a pointer to single populated ZitatespuckerZitat.
+	idx refers to the array index within ZitatArray.
+	NULL on error.
+
+	This function allocates, and the given object must be freed with ZitatespuckerZitatFree().
+*/
+static ZitatespuckerZitat *ZitatespuckerJSONGetZitatSingle(json_t *ZitatArray, const size_t idx);
+
+
 /* Externally callable */
 
 size_t ZitatespuckerJSONGetAmountFromFile(const char *filename)
+{
+	json_t *zitatscope = ZitatespuckerJSONGetZitatArrayFromFile(filename);
+
+	size_t ret = 0;
+	if (zitatscope != NULL) {
+		ret = json_array_size(zitatscope);
+		json_decref(zitatscope);
+	}
+	
+	return ret;
+}
+
+ZitatespuckerZitat *ZitatespuckerJSONGetZitatSingleFromFile(const char *filename, const size_t idx)
+{
+	json_t *zitatscope = ZitatespuckerJSONGetZitatArrayFromFile(filename);
+	if (zitatscope == NULL)
+		return NULL;
+	else {
+		ZitatespuckerZitat *ret = ZitatespuckerJSONGetZitatSingle(zitatscope, idx);
+		json_decref(zitatscope);
+		return ret;
+	}
+}
+
+ZitatespuckerZitat *ZitatespuckerJSONGetZitatAllFromFile(const char *filename)
+{
+	json_t *ZitatArray;
+	if ((ZitatArray = ZitatespuckerJSONGetZitatArrayFromFile(filename)) == NULL)
+		return NULL;
+	else {
+		size_t len = json_array_size(ZitatArray);
+		
+		ZitatespuckerZitat *ret = ZitatespuckerJSONGetZitatSingle(ZitatArray, 0);
+		if (ret != NULL) {
+			size_t i = 1;
+			ZitatespuckerZitat *cur = ret;
+			ZitatespuckerZitat *prev;
+			for ( ; i < len; i++) {
+				cur->nextZitat = ZitatespuckerJSONGetZitatSingle(ZitatArray, i);
+				if (cur->nextZitat != NULL) {
+					prev = cur;
+					cur = cur->nextZitat;
+					cur->prevZitat = prev;
+				} else
+					break;
+			}
+		}
+		json_decref(ZitatArray);
+		return ret;
+	}
+}
+
+
+/* Static function definitions */
+
+static json_t *ZitatespuckerJSONGetZitatArrayFromFile(const char *filename)
 {
 	json_error_t err;
 	json_t *globalscope = json_load_file(filename, 0, &err); // remember: reference count
@@ -38,7 +114,7 @@ size_t ZitatespuckerJSONGetAmountFromFile(const char *filename)
 		#ifndef ZITATESPUCKER_NOPRINT
 		(void) fprintf(stderr, "%s:%d:%s: json_load_file() failed:\n%s", __FILE__, __LINE__, __func__, err.text);
 		#endif
-		return 0;
+		return NULL;
 	}
 
 	json_t *zitatscope = json_object_get(globalscope, ZITATESPUCKERZITATKEYNAME);
@@ -47,7 +123,7 @@ size_t ZitatespuckerJSONGetAmountFromFile(const char *filename)
 		#ifndef ZITATESPUCKER_NOPRINT
 		(void) fprintf(stderr, "%s:%d:%s: json_object_get() failed:\nMaybe the key %s does not exist?", __FILE__, __LINE__, __func__, ZITATESPUCKERZITATKEYNAME);
 		#endif
-		return 0;
+		return NULL;
 	}
 
 	json_incref(zitatscope);
@@ -59,11 +135,8 @@ size_t ZitatespuckerJSONGetAmountFromFile(const char *filename)
 		#ifndef ZITATESPUCKER_NOPRINT
 		(void) fprintf(stderr, "%s:%d:%s: Key %s is not an array!\n", __FILE__, __LINE__, __func__, ZITATESPUCKERZITATKEYNAME);
 		#endif
-		return 0;
+		return NULL;
 	}
 
-	size_t ret = json_array_size(zitatscope);
-	json_decref(zitatscope);
-
-	return ret;
+	return zitatscope;
 }
